f50b40b4a43b164fb166e75ceda98c05f541053a
moosewall <m.sw2005@gmail.com>
2013-11-10 16:42:04 -0500

    More XML work

--- MarksJavaCommon/src/XmlUtils.java
+++ MarksJavaCommon/src/XmlUtils.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,11 +16,16 @@ import javax.xml.transform.stream.StreamResult;
  
 
 
+
+
+
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
  
 
 public class XmlUtils 
@@ -35,8 +43,11 @@ public class XmlUtils
 		String sOutFile = "C:\\file.xml" ;
 		sOutFile = AppState.m_AppDetails.m_sMainClassDir + "\\test_XmlWrite.xml" ;
 
+		/*
 		test_XmlWrite (sOutFile) ;
 		test_ReadXml (sOutFile) ;
+		*/
+		test_Object_SER () ;
 	}
 	/////////////////////////////////////////
 	//
@@ -286,5 +297,230 @@ public class XmlUtils
 	        	s = "Skipping" ;
 	        }
 	    }
-	}	
+	}
+	//////////////////////////////////////
+	//Classes to manage DOM XML serialization of based types.
+	//
+	//http://docs.oracle.com/javase/tutorial/java/IandI/subclasses.html
+	//
+	public static void test_Object_SER ()
+	{
+		String s = "" ;
+		try
+		{
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+	 
+			// root elements
+			Document doc = docBuilder.newDocument();
+			Element rootEle = doc.createElement("TestDataObject");
+			Node rootNode = (Node)rootEle ;
+			doc.appendChild(rootNode);
+			
+			/////////////////////////////
+			XmlUtils xu = new XmlUtils () ;
+			String_SER ssTest1 = xu.new String_SER ("ssTest1", "ssTest1 Val") ;
+			ssTest1.SaveToXml(doc, rootNode);
+			/////////////////////////////
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+
+			StringWriter swXml = new StringWriter () ;
+			StreamResult result = new StreamResult(swXml);
+			
+			/////
+			//write out the XML so readable in text editor per this:
+			//http://stackoverflow.com/questions/161462/java-writing-a-dom-to-an-xml-file-formatting-issues
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			/////
+			
+			transformer.transform(source, result);
+			String sXml = swXml.toString() ;
+
+			StringReader srRdr = new StringReader (sXml) ;
+			InputSource isRdr = new InputSource(srRdr);
+			Document docRead = docBuilder.parse(isRdr);
+			docRead.normalize();
+
+			docRead.getDocumentElement().normalize();
+			Node nRootRead = docRead.getDocumentElement() ;			
+			String_SER ssTest1_read = xu.new String_SER ("ssTest1", "ssTest1 Val") ;
+			s = ssTest1_read.sReadFromXml(nRootRead) ;
+			
+		}
+		catch (Exception exp)
+		{
+			
+		}
+	}
+	//////////////////////////////////
+	//Class to prototype XML serializable classes
+	//
+	public class Object_SER_TestClass
+	{
+		public String m_sVal1 = "" ;
+		public String m_sVal2 = "" ;
+		public int m_iVal1 = 1 ;
+		public boolean m_bVal1 = true ;
+		public List <String> m_lstStrs = new ArrayList<String>() ;
+		
+		public String sSaveToXml ()
+		{
+			String sr = "" ;
+			try
+			{
+				XmlUtils xu = new XmlUtils () ;
+				List<Object_SER> list = new ArrayList<Object_SER>();
+
+				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		 
+				// root elements
+				Document doc = docBuilder.newDocument();
+				Element rootEle = doc.createElement("Object_SER_TestClass");
+				Node rootNode = (Node)rootEle ;
+				doc.appendChild(rootNode);
+				
+				String_SER ss = null ;
+				int_SER is = null ;
+				bool_SER bs = null ;
+				
+				ss = xu.new String_SER ("m_sVal1", m_sVal1) ;
+				list.add(ss) ;
+			
+				ss = xu.new String_SER ("m_sVal2", m_sVal2) ;
+				list.add(ss) ;
+				
+				is = xu.new int_SER ("m_iVal1", m_iVal1) ;
+				list.add(is) ;
+				
+				bs = xu.new bool_SER ("m_bVal1", m_bVal1) ;
+				list.add(bs) ;
+				
+				/*todo
+				for (int iS=0; is<m_lstStrs.size(); iS++)
+				{
+					ss = new String_SER ("m_lstStrs")
+				}
+				*/
+				
+				for (int iOs=0; iOs<list.size(); iOs++)
+				{
+					Object_SER os = list.get(iOs) ;
+					os.SaveToXml(doc, rootNode);
+				}
+				
+				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+				Transformer transformer = transformerFactory.newTransformer();
+				DOMSource source = new DOMSource(doc);
+
+				StringWriter swXml = new StringWriter () ;
+				StreamResult result = new StreamResult(swXml);
+				
+				/////
+				//write out the XML so readable in text editor per this:
+				//http://stackoverflow.com/questions/161462/java-writing-a-dom-to-an-xml-file-formatting-issues
+				transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+				/////
+				
+				transformer.transform(source, result);
+				String sXml = swXml.toString() ;
+				sr = sXml ;
+			}
+			catch (Exception exp)
+			{
+			}
+			
+			return sr ;
+		}
+	}
+	public class Object_SER
+	{
+		protected String m_sValName = "" ;
+		protected String m_sValType = "" ;
+		protected String m_sVal = "" ;
+		
+		public Object_SER (String sValName, String sValType, String sVal)
+		{
+			m_sValName = sValName ;
+			m_sValType = sValType ;
+			m_sVal = sVal ;
+		}
+		public void SaveToXml (Document doc, Node node)
+		{
+			Element eleNew = doc.createElement(m_sValName);
+			node.appendChild(eleNew);
+	 
+			Attr attr = null ;
+
+			attr = doc.createAttribute("type");
+			attr.setValue(m_sValType);
+			eleNew.setAttributeNode(attr);
+			
+			attr = doc.createAttribute("value");
+			attr.setValue(m_sVal);
+			eleNew.setAttributeNode(attr);
+		}
+		public String sReadFromXml (Node node)
+		{
+			String sr = "" ;
+			
+			NodeList nlChildren = node.getChildNodes() ;
+			if (nlChildren != null)
+			{
+				for (int iChild=0; iChild<nlChildren.getLength(); iChild++)
+				{
+					Node nChild = nlChildren.item(iChild) ;
+					if (nChild.getNodeType() == Node.ELEMENT_NODE)
+					{
+						if (nChild.getNodeName() == m_sValName)
+						{
+							sr = nChild.getAttributes().getNamedItem("value").getNodeValue() ;
+							break ;
+						}
+					}
+				}
+			}
+			
+			return sr ;
+		}
+	}
+	public class String_SER extends Object_SER 
+	{
+		public String_SER (String sValName, String sVal)
+		{
+			super (sValName, "String", sVal) ;
+		}
+	}
+	public class int_SER extends Object_SER 
+	{
+		public int_SER (String sValName, int iVal)
+		{
+			super (sValName, "int", "") ;
+			m_sVal = Integer.toString(iVal) ;
+		}
+		public int iReadFromXml (Node node)
+		{
+			String sr = sReadFromXml (node) ;
+			int ir = Integer.getInteger(sr) ;
+			return ir ;
+		}
+	}
+	public class bool_SER extends Object_SER 
+	{
+		public bool_SER (String sValName, Boolean bVal)
+		{
+			super (sValName, "int", "") ;
+			m_sVal = String.valueOf(bVal) ;
+		}
+		public Boolean bReadFromXml (Node node)
+		{
+			String sr = sReadFromXml (node) ;
+			Boolean br = Boolean.valueOf(sr) ;
+			return br ;
+		}
+	}
+	
 }
