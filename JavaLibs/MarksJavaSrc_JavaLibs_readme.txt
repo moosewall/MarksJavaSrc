a21ab13994790223de95e72cefa67049fef95b2f
mwallace <wallace.mark@mail.navy.mil>
2014-03-16 10:35:39 -0400

    Created MarksEwsTests and added supporting libraries to test Exchange Web Services (EWS) api

---
+++ JavaLibs/MarksJavaSrc_JavaLibs_readme.txt
@@ -0,0 +1,20 @@
+
+
+3/16/2014, MW, Downloading jars for Exchange Web Services (EWS) api
+
+commons-httpclient-3.1.jar from
+http://mavenhub.com/c/org/apache/commons/httpclient/HttpConnectionManager/jar
+
+
+commons-codec-1.4.jar
+from
+http://www.findjar.com/jar/commons-codec/commons-codec/1.4/commons-codec-1.4.jar.html;jsessionid=1050E6401EFFAB66325EC5F44DC3B440
+
+jcifs-1.3.15.jar
+from
+http://repo.jenkins-ci.org/releases/org/samba/jcifs/jcifs/1.3.15/
+
+commons-logging-1.1.1.jar
+from
+http://mvnrepository.com/artifact/commons-logging/commons-logging/1.1.1
+
---
+++ MarksEwsTests/src/MarksEwsTest1_ConsoleApp.java
@@ -0,0 +1,113 @@
+
+import microsoft.exchange.webservices.*;
+import microsoft.exchange.webservices.data.ExchangeService;
+import microsoft.exchange.webservices.data.ExchangeVersion;
+
+/*
+ MW, 3/15/2014, attempting to use the EWS API.
+ 
+ 
+ */
+public class MarksEwsTest1_ConsoleApp {
+	public static void main(String[] args) throws Exception 
+	{
+		//HelloWorld () ;
+		
+		/*
+		//TraceUtils.test1 () ;
+		FileUtils.test1();  
+		//TraceUtils.TestStackTrace () ;
+		 
+		 */
+		
+		AppState.initAppDetails(MarksEwsTest1_ConsoleApp.class); //call this before anything!
+		
+		String sFN = TraceUtils.sGetFN () ;
+		AppLog.Log(sFN + " initialized");
+		
+		// TODO Auto-generated method stub
+
+		//StdioLogger.Log("test");
+	
+		/*
+		MarksTestJava1_AppSettings settings = new MarksTestJava1_AppSettings () ;
+		settings.ReadSettingsFromIni();
+		settings.WriteSettingsToIni();  //write back so a stub .ini exists
+		
+		settings.CheckMe();
+		
+		JavaTests.StringTests();
+		JavaTests.CollectionTests();
+		
+		CryptUtils.test1();
+		*/
+		/*
+		 * JavaTests.FileTests();
+		 */
+		
+		TestEws () ;
+		
+		AppLog.Log(sFN + " exited");
+	}
+	/////////////////////////////////////////
+	//3/15/2014
+	//
+	public static void TestEws ()
+	{
+		String sFN = TraceUtils.sGetFN () ;
+
+		String sLog = "" ;
+		try
+		{
+			ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+			
+			String sExchangeUrl = "https://webmail.east.nmci.navy.mil/owa/" ;
+			service.autodiscoverUrl(sExchangeUrl) ;
+		}
+		catch (Exception exp)
+		{
+			sLog = sFN + " exception " + exp.getMessage() ;
+			AppLog.Log(sLog);
+		}
+	}
+	public static void HelloWorld ()
+	{
+		System.out.println("Hello World!");
+	}
+	/*
+	////////////////////////////
+	//
+	public static void Init ()
+	{
+		String sFN = TraceUtils.sGetFN() ;
+		String sLog = "" ;
+		
+		FileUtils.m_sMainClassDir = sGetClassDir () ;
+		FileUtils.m_sMainClassName = sGetClassName () ;
+		
+		sLog = sFN + "Class " + FileUtils.m_sMainClassName + " in " + FileUtils.m_sMainClassDir + " loading..." ;
+		TraceUtils.Trc(sLog);
+	}
+	/////////////////////////////
+	//
+	public static String sGetClassDir ()
+	{
+		String sr = "" ;
+		
+		sr = MainConsole.class.getProtectionDomain().getCodeSource().getLocation().getPath() ;
+		
+		return sr ;
+	}
+	/////////////////////////////
+	//
+	public static String sGetClassName ()
+	{
+		String sr = "" ;
+		
+		String sName = MainConsole.class.getName();
+		sr = sName ;
+		
+		return sr ;
+	}
+	*/
+}
